7970c58c7deded04ce841f26e5f8c2afc2895003
Mathenge-Alex <arxmaina@gmail.com>
2022-04-18 15:47:10 +0300

    add github base url

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  BASE_URL: string = 'https://mathenge-alex.github.io/SearchGithub/'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
