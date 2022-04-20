9d73b1053321a408685ad2efb43f2452eaaba1ee
Mathenge-Alex <arxmaina@gmail.com>
2022-04-20 22:01:50 +0300

    add functionality code to feature highlight directive

--- src/app/featuring.directive.ts
+++ src/app/featuring.directive.ts
@@ -1,10 +1,17 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appFeaturing]'
 })
 export class FeaturingDirective {
 
-  constructor() { }
+  constructor(element: ElementRef) {
+    }
+    @HostListener("click") onClicks(){
+      this.deco("underline")
+    }
+  private deco(action:string){
+    this.element.nativeElement.style.color = action;
+  }
 
 }
