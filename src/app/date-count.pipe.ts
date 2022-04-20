03680375dc01cc620b39e7a1321b7c57eb0c5a47
Mathenge-Alex <arxmaina@gmail.com>
2022-04-20 23:34:39 +0300

    add functionality to dating pipe

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    let currentDate: Date = new Date();
+    let fullCurrentDate: any = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate())
+    var dateDifference = Math.abs(value - fullCurrentDate) // date diff in ms
+    const timeInSeconds = 86400; // seconds in a day
+    var pastSeconds = dateDifference * 0.001; //convert ms to seconds
+    var dater = pastSeconds / timeInSeconds;
+
+    if (dater >= 1 && value < fullCurrentDate) {
+        var days = dater/365
+      return days;
+    } else {
+      return 0;
+    }
   }
 
 }
