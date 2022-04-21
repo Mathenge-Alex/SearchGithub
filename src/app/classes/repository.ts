3dcc5a85d611318066d8aec88373618226e36a22
Mathenge-Alex <arxmaina@gmail.com>
2022-04-21 07:36:14 +0300

    add user and repository constructors.

--- src/app/classes/repository.ts
+++ src/app/classes/repository.ts
@@ -1,2 +1,5 @@
 export class Repository {
+    constructor(
+        public repos_url:string, public public_repos:number,public description:string,public language:string, public created_at:Date
+    ){}
 }
--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,5 +1,5 @@
 export class User {
   constructor(
-    public avatar_url:string, public repos_url:string, public name:string, public html_url:string, bio:string, login:string, public following:number, public followers:number, public public_repos:number, public created_at:Date
+    public avatar_url:string, public name:string, public html_url:string, public bio:string, public login:string, public following:number, public followers:number, 
   ){}
 }
